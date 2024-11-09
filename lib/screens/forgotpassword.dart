9b16b13e9c0aa435d0a62775d72f05d21ccec883
Nabeel N <nabeelusmannanakkal@gmail.com>
2024-11-09 22:04:45 +0530

    feat: Forgotpassword functionalty completed

--- lib/screens/forgotpassword.dart
+++ lib/screens/forgotpassword.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:food_app/screens/signup.dart';
 import 'package:food_app/widget/widget_support.dart';
@@ -11,6 +12,22 @@ class ForgotPassword extends StatefulWidget {
 
 class _ForgotPasswordState extends State<ForgotPassword> {
   TextEditingController emailController = TextEditingController();
+  String email = '';
+  final _formKey = GlobalKey<FormState>();
+
+  resetPassword() async {
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Password reset email has been sent')));
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text('Enter your valid email')));
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,54 +53,71 @@ class _ForgotPasswordState extends State<ForgotPassword> {
             ),
             Expanded(
                 child: Form(
+                    key: _formKey,
                     child: Padding(
-              padding: EdgeInsets.only(left: 20.0, right: 20.0),
-              child: ListView(
-                children: [
-                  TextFormField(
-                    controller: emailController,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your email';
-                      }
-                      return null;
-                    },
-                    decoration: const InputDecoration(
-                        hintText: 'Enter your email',
-                        prefixIcon: Icon(
-                          Icons.person_outlined,
-                          size: 30.0,
-                        ),
-                        border: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10.0)))),
-                  ),
-                  const SizedBox(
-                    height: 30.0,
-                  ),
-                  ElevatedButton(
-                      style: const ButtonStyle(
-                        elevation: WidgetStatePropertyAll(10.0),
-                        padding: WidgetStatePropertyAll(EdgeInsets.only(top: 15.0,bottom: 15.0)),
-                          backgroundColor: WidgetStatePropertyAll(
-                              Color.fromARGB(255, 244, 133, 54))),
-                      onPressed: () {},
-                      child: Text(
-                        'Confirm',
-                        style: AppWidget.buttonBoldTextStyle(),
-                      )),
-                      const SizedBox(height: 40.0,),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                            return const SignUp();
-                          },));
-                        },
-                        child: Center(child: Text("Don't have an account? Create",style: AppWidget.semiBoldText2Style(),)),
-                      )
-                ],
-              ),
-            )))
+                      padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                      child: ListView(
+                        children: [
+                          TextFormField(
+                            controller: emailController,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your email';
+                              }
+                              return null;
+                            },
+                            decoration: const InputDecoration(
+                                hintText: 'Enter your email',
+                                prefixIcon: Icon(
+                                  Icons.person_outlined,
+                                  size: 30.0,
+                                ),
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.all(
+                                        Radius.circular(10.0)))),
+                          ),
+                          const SizedBox(
+                            height: 30.0,
+                          ),
+                          ElevatedButton(
+                              style: const ButtonStyle(
+                                  elevation: WidgetStatePropertyAll(10.0),
+                                  padding: WidgetStatePropertyAll(
+                                      EdgeInsets.only(top: 15.0, bottom: 15.0)),
+                                  backgroundColor: WidgetStatePropertyAll(
+                                      Color.fromARGB(255, 244, 133, 54))),
+                              onPressed: () {
+                                if(_formKey.currentState!.validate()){
+                                  setState(() {
+                                    email = emailController.text;
+                                  });
+                                }
+                                resetPassword();
+                              },
+                              child: Text(
+                                'Confirm',
+                                style: AppWidget.buttonBoldTextStyle(),
+                              )),
+                          const SizedBox(
+                            height: 40.0,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) {
+                                  return const SignUp();
+                                },
+                              ));
+                            },
+                            child: Center(
+                                child: Text(
+                              "Don't have an account? Create",
+                              style: AppWidget.semiBoldText2Style(),
+                            )),
+                          )
+                        ],
+                      ),
+                    )))
           ],
         ),
       ),
